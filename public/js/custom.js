4678070ebde4d0b9324060119f980110b3cbd43c
honza.pofider@seznam.cz <honza.pofider@seznam.cz>
2015-06-25 20:34:59 +0200

    add deep dive video

--- public/js/custom.js
+++ public/js/custom.js
@@ -5,12 +5,12 @@
                 overlay: true,
                 shadow: true,
                 flat: true,
-                title: 'jsreport - Quick Start Tutorial',
+                title: 'jsreport - Deep Dive',
                 padding: 10,
                 content: '',
                 onShow: function(_dialog) {
                     var content = _dialog.children('.content');
-                    content.html('<iframe width="853" height="480" src="//www.youtube.com/embed/L7MZqwDCxP8" frameborder="0" allowfullscreen></iframe><p class="modal">This video is very basic quick start into reporting software <strong>jsreport</strong>.</p>');
+                    content.html('<iframe width="853" height="480" src="//www.youtube.com/embed/fhOQ0HPjK6s" frameborder="0" allowfullscreen></iframe><p class="modal"></p>');
                 }
             });
         });
