7dbf29f37e60816670ec9d6980c46632b56b5e5e
pofider <honza.pofider@seznam.cz>
2021-05-31 22:54:03 +0200

    add github token to env

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
+TRAVIS_TAG = "1.0.0"
 printenv
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 docker build -t website .
-docker tag website jsreport/website:1.0.0
+docker tag website jsreport/website:$TRAVIS_TAG
 docker push jsreport/website
 
 git clone https://github.com/pofider/kubernetes.git
